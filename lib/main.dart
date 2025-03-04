cd6f1857acb37eeeacad79a99066240b41efd164
Pawan70056 <ssmp70056@gmail.com>
2025-03-04 22:18:07 +0545

    changes in code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:softwarica_student_management_bloc/app/app.dart';
-import 'package:softwarica_student_management_bloc/app/di/di.dart';
-import 'package:softwarica_student_management_bloc/core/network/hive_service.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:timberr/constants.dart';
+import 'package:timberr/private_key.dart' as private_key;
+import 'package:timberr/screens/authentication/login_screen.dart';
+import 'package:timberr/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await HiveService.init();
 
-  // await HiveService().clearStudentBox();
+  try {
+    await Supabase.initialize(
+      url: private_key.supabaseUrl,
+      anonKey: private_key.supabaseAnonKey,
+    );
+    debugPrint("✅ Supabase initialized successfully.");
+  } catch (e) {
+    debugPrint("❌ Error initializing Supabase: $e");
+  }
 
-  await initDependencies();
-
-  runApp(
-    App(),
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
+
+  runApp(const TimberrApp());
+}
+
+class TimberrApp extends StatelessWidget {
+  const TimberrApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/', // ✅ Ensuring correct initial route
+      getPages: [
+        GetPage(name: '/', page: () => Wrapper()), // ✅ Root route
+        GetPage(name: '/login', page: () => LoginScreen()), // ✅ Login route
+      ],
+      theme: ThemeData(
+        useMaterial3: false,
+        fontFamily: "NunitoSans",
+        appBarTheme: const AppBarTheme(color: Colors.white, elevation: 0),
+        scaffoldBackgroundColor: Colors.white,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: kOffBlack),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/private_key.dart
@@ -0,0 +1,5 @@
+// lib/private_keys.dart
+
+const supabaseUrl = 'https://vrlqojibyggrwavmdldt.supabase.co';
+const supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZybHFvamlieWdncndhdm1kbGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MDcyMDcsImV4cCI6MjA1NjQ4MzIwN30.WzGSrE0gean4BwyO9firBujemRfqysFQO9IkOcI4szc';
